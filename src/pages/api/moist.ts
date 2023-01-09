10e8e3f5e710f9072ec5b472dfff3e061adfa79e
Freddiiy <frederikgaller@live.dk>
2023-01-09 15:19:10 +0100

    asd

--- src/pages/api/moist.ts
+++ src/pages/api/moist.ts
@@ -5,10 +5,9 @@ export interface MoistRes {
 }
 
 const moist = async (req: NextApiRequest, res: NextApiResponse<MoistRes>) => {
-    const waterRes = await axios.get("http://192.168.2.70:9000/moist");
+    const waterRes = await axios.get<M>("http://192.168.2.70:9000/moist");
     const data = await waterRes.data;
     if (waterRes.status == 200) {
-        console.log(data);
         res.status(200).json({
             responseText: data,
         });
