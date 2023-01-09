5b251b468e6e76c705c3886b2910461bb5d6a4c2
Freddiiy <frederikgaller@live.dk>
2023-01-09 13:21:26 +0100

    asd

--- src/pages/api/water.ts
+++ src/pages/api/water.ts
@@ -5,7 +5,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const water = async (req: NextApiRequest, res: NextApiResponse) => {
     const waterRes = await axios.get("http://localhost:9000/water");
     const data = await waterRes.data;
-    res.status(200).json(data);
+    if (waterRes.status == 200) {
+        res.status(200).json(data);
+    } else {
+        res.status(500);
+    }
 };
 
 export default water;
