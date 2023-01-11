5c73c356b844d7543df0da779720304ce27d4dce
Freddiiy <frederikgaller@live.dk>
2023-01-11 14:07:55 +0100

    asd

--- src/pages/api/moist.ts
+++ src/pages/api/moist.ts
@@ -1,22 +1,30 @@
 import axios from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiRequest, NextApiResponse} from "next";
 import {prisma} from "../../server/db/client";
+
 export interface MoistRes {
     responseText: string,
 }
 
 const moist = async (req: NextApiRequest, res: NextApiResponse) => {
-    const moist = req.body.moisturePercent;
-    const moistRes = await prisma.moistData.create({
-        data: {
-            value: moist,
-        }
-    });
+    if (req.method == "POST") {
 
-    if (moistRes) {
-        res.status(200);
+        const moist = req.body.moisturePercent;
+        const moistRes = await prisma.moistData.create({
+            data: {
+                value: moist,
+            }
+        });
+
+        if (moistRes) {
+            res.status(200);
+        } else {
+            res.status(500);
+        }
     } else {
-        res.status(500);
+        res.status(400).json({
+            responseText: "Please use POST."
+        })
     }
 };
 
