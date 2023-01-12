9701cbe2afd63519062a5c82f7801818bff0a5ab
Freddiiy <frederikgaller@live.dk>
2023-01-12 20:22:06 +0100

    asd

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -6,7 +6,7 @@ interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes
 
 const ActionButton = (props: ButtonProps) => {
     return (
-        <button {...props} className={"p-2 bg-purple-600 w-full hover:bg-purple-700"}>
+        <button {...props} className={"p-2 bg-indigo-600 w-full hover:bg-indigo-700"}>
             <span className={"font-semibold text-lg flex justify-center items-center text-white"}>
                 {props.children}
             </span>
